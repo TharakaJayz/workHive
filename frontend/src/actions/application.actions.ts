6f1171fa2a2c32b1c21424fc33a69c4f18a06691
tharaka prabhath <tharakaprabhath@tharakas-MacBook-Pro.local>
2026-06-22 12:52:35 +0530

    add backend docker setup

--- frontend/src/actions/application.actions.ts
+++ frontend/src/actions/application.actions.ts
@@ -1,28 +1,28 @@
-"use server";
+// "use server";
 
-import prisma from "@/lib/prisma";
-import { createApplicationSchema } from "@/lib/validations/application";
-import { ApplicationStatus } from "@/shared/enum";
+// import prisma from "@/lib/prisma";
+// import { createApplicationSchema } from "@/lib/validations/application";
+// import { ApplicationStatus } from "@/shared/enum";
 
 
-export async function createJobApplication(payload: any, userId: number) {
+// export async function createJobApplication(payload: any, userId: number) {
 
-  try {
+//   try {
 
-    const { job_id, ...validatedData } = createApplicationSchema.parse(payload);
-    const newApplication = await prisma.application.create({
-      data: {
-        job_id: job_id,
-        resume_url: validatedData.resume_url,
-        cover_letter: validatedData.cover_letter || null,
-        user_id: userId,
-        status: ApplicationStatus.PENDING,
-      },
-    });
+//     const { job_id, ...validatedData } = createApplicationSchema.parse(payload);
+//     const newApplication = await prisma.application.create({
+//       data: {
+//         job_id: job_id,
+//         resume_url: validatedData.resume_url,
+//         cover_letter: validatedData.cover_letter || null,
+//         user_id: userId,
+//         status: ApplicationStatus.PENDING,
+//       },
+//     });
 
-    return { success: true, data: newApplication };
-  } catch (error: any) {
-    console.error("DB Write Error:", error);
-    return { success: false, error: error.message || "Failed to submit application." };
-  }
-}
+//     return { success: true, data: newApplication };
+//   } catch (error: any) {
+//     console.error("DB Write Error:", error);
+//     return { success: false, error: error.message || "Failed to submit application." };
+//   }
+// }
